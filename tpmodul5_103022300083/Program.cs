b7fb42bf644ad36fff23391019166d02f447ca32
JidanAzizi <idanaf3@gmail.com>
2025-03-16 23:05:48 +0700

    Final Commit

--- tpmodul5_103022300083/Program.cs
+++ tpmodul5_103022300083/Program.cs
@@ -1,5 +1,7 @@
-﻿class main {
+class main {
     static void Main(String[] args) {
+        haloGeneric nama = new haloGeneric();
+        nama.sapaUser<String>("Jidan");
         dataGeneric<String> z = new dataGeneric<string>("103022300083");
         z.printData();
     }
@@ -16,3 +18,13 @@ class dataGeneric<Z> {
         Console.WriteLine("Data yang tersimpan adalah: " + data);
     }
 }
+
+
+class haloGeneric()
+{
+    public void sapaUser<Z>(Z user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
+
